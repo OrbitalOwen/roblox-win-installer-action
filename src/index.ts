5cdd5ed3357a8200c5151335aac16de563ba2cd9
OrbitalOwen <44505297+OrbitalOwen@users.noreply.github.com>
2020-11-14 10:27:20 +0000

    Setup

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import core from "@actions/core";
+import exec from "@actions/exec";
+import { getOctokit } from "@actions/github";
+import { downloadTool, extractZip } from "@actions/tool-cache";
+
+const GITHUB_USER = "OrbitalOwen";
+const REPO_NAME = "roblox-win-installer";
+
+const cookie = core.getInput("cookie");
+const version = core.getInput("version");
+const githubToken = core.getInput("token");
+
+async function getRelease() {
+	const octokit = getOctokit(githubToken);
+
+	if (version) {
+		const response = await octokit.repos.listReleases({
+			owner: GITHUB_USER,
+			repo: REPO_NAME,
+		});
+
+		for (const release of response.data) {
+			if (release.tag_name === version) {
+				return release;
+			}
+		}
+		throw new Error(
+			`${GITHUB_USER}/${REPO_NAME} release ${version} not found`
+		);
+	} else {
+		const response = await octokit.repos.getLatestRelease({
+			owner: GITHUB_USER,
+			repo: REPO_NAME,
+		});
+
+		return response.data;
+	}
+}
+
+async function downloadRelease() {
+	const release = await getRelease();
+
+	const zipPath = await downloadTool(release.zipball_url);
+	const extractedPath = await extractZip(zipPath);
+
+	return extractedPath;
+}
+
+async function install() {}
+
+downloadRelease().catch((error) => {
+	core.setFailed(error.message);
+});
